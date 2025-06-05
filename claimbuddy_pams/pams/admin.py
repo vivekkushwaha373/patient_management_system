471d46071407542099f849d9b66d8aa2eabbf72f
MyGit <vivekkushwaha734@gmail.com>
2025-06-05 14:24:14 +0530

    register model in admin

--- claimbuddy_pams/pams/admin.py
+++ claimbuddy_pams/pams/admin.py
@@ -1,3 +1,24 @@
-from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Patient
+
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ['full_name', 'age', 'gender', 'insurance_provider', 'policy_number', 'created_at']
+    list_filter = ['gender', 'insurance_provider', 'created_at']
+    search_fields = ['full_name', 'policy_number', 'insurance_provider']
+    readonly_fields = ['created_at', 'updated_at']
+    
+    fieldsets = (
+        ('Personal Information', {
+            'fields': ('full_name', 'age', 'gender')
+        }),
+        ('Insurance Information', {
+            'fields': ('insurance_provider', 'policy_number')
+        }),
+        ('Timestamps', {
+            'fields': ('created_at', 'updated_at'),
+            'classes': ('collapse',)
+        }),
+    )
